d0f0d6a42b1f582c8fd08b671f2fccbb549492e2
Aniket  Joshi <aniket.joshi6789@gmail.com>
2026-07-21 12:32:47 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #iterate over the grid, and if you see a 1, perform a dfs to count the islands
+        #after finishing the search then only increment the count
+        ans, M, N = 0, len(grid), len(grid[0])
+        def solve(r, c):
+            if r < 0 or c < 0 or r >=M or c >= N or grid[r][c]!="1":
+                return
+            # temp = grid[r][c]
+            grid[r][c]="#"
+            solve(r+1, c)
+            solve(r-1, c)
+            solve(r, c+1)
+            solve(r, c-1)
+            # grid[r][c] = temp
+
+        for r in range(M):
+            for c in range(N):
+                if grid[r][c]=="1":
+                    ctr=0
+                    solve(r, c)
+                    ans+=1
+        return ans
